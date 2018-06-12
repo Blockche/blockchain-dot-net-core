8d47258043c34494e8d1102542ee668d690c21c5
KristianMariyanov <kristian.mariyanov@gmail.com>
2018-06-12 20:56:57 +0300

    Added empty asp.net core project

---
+++ Wallet/Blockche.Wallet.Web/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Blockche.Wallet.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
