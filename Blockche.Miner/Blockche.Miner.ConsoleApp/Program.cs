1b33bf742f6c688dce206ede230264b5f74ba864
Dzhenko Penev <dzhenko@gmail.com>
2018-06-12 20:42:41 +0300

    Initial miner project added.

---
+++ Blockche.Miner/Blockche.Miner.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blockche.Miner.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
