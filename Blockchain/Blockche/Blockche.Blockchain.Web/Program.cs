811a8a6b4618c99c6d5870b8c51d91d0d6921989
Boncho Valkov <bonchovylkov@abv.bg>
2018-06-13 12:22:35 +0300

    add hosting json

--- Blockchain/Blockche/Blockche.Blockchain.Web/Program.cs
+++ Blockchain/Blockche/Blockche.Blockchain.Web/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Blockche.Blockchain.Web
 {
@@ -14,11 +11,26 @@ namespace Blockche.Blockchain.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
+                .UseConfiguration(config)
+                  .Configure(app =>
+                  {
+                      app.Run(context =>
+                          context.Response.WriteAsync("Hello, World!"));
+                  })
+                .Build();
+        }
     }
 }
