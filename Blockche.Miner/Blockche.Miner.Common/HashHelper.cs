2a320c5519670236247447f1fbc418041c580fbc
Dzhenko Penev <dzhenko@gmail.com>
2018-06-14 21:50:55 +0300

    Migrating to dotnet core done.

---
+++ Blockche.Miner/Blockche.Miner.Common/HashHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blockche.Miner.Common
+{
+    public class HashHelper
+    {
+        private static readonly Lazy<SHA256Managed> sHA256 = new Lazy<SHA256Managed>(true);
+
+        public static string ComputeSHA256(string input)
+            => BitConverter.ToString(sHA256.Value.ComputeHash(Encoding.UTF8.GetBytes(input ?? string.Empty))).Replace("-", string.Empty).ToLowerInvariant();
+    }
+}
---
+++ Blockche.Miner/Blockche.Miner.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blockche.Miner.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
