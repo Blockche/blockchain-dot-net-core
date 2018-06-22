62fd55b0639d04d3e427512976272e125e46e420
bonchovylkov <bonchovylkov>
2018-06-22 14:53:06 +0300

    add view for the addresses infomation + controller

---
+++ Blockchain/Blockche/Blockche.Blockchain.Web/Controllers/AddressController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Blockche.Blockchain.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blockche.Blockchain.Web.Controllers
+{
+    public class AddressController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Details(string id)
+        {
+            AddressViewModel model = new AddressViewModel();
+            var node = this.GetNodeSingleton();
+            model.Balance = node.Chain.GetAccountBalance(id);
+            model.TransFrom = node.Chain.GetAllTransactions().Where(s => s.From == id).ToList();
+            model.TransTo = node.Chain.GetAllTransactions().Where(s => s.To == id).ToList();
+
+            return this.View(model);
+        }
+    }
+}
