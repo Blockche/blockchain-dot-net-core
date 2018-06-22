fd7aefd5776d318ff8c10245d9907d7e3d41accc
KristianMariyanov <kristian.mariyanov@gmail.com>
2018-06-22 16:09:53 +0300

    added block transactions action and view

--- Blockchain/Blockche/Blockche.Blockchain.Web/Controllers/BlocksController.cs
+++ Blockchain/Blockche/Blockche.Blockchain.Web/Controllers/BlocksController.cs
@@ -8,7 +8,24 @@ namespace Blockche.Blockchain.Web.Controllers
         {
             var node = this.GetNodeSingleton();
             var block = node.Chain.GetBlockByIndex(id);
+            if (block == null)
+            {
+                return this.RedirectToAction("Index", "Explorer"); // TODO: add error temp data and visualize it
+            }
             return this.View(block);
+        }
+
+        public IActionResult Txs(int id)
+        {
+            var node = this.GetNodeSingleton();
+            var block = node.Chain.GetBlockByIndex(id);
+
+            if (block == null)
+            {
+                return this.RedirectToAction("Index", "Explorer"); // TODO: add error temp data and visualize it
+            }
+
+            return this.View(block.Transactions);
         }
     }
 }
