f4ae43bc0e042f4a5c2c6274cadac5308ee9f4f4
KristianMariyanov <kristian.mariyanov@gmail.com>
2018-06-23 10:12:55 +0300

    ui and ux improvements; Added balance check of the current account

---
+++ Blockchain/Blockche/Blockche.Wallet.Web/Controllers/BalansesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Blockche.Blockchain.Common;
+using Blockche.Blockchain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blockche.Wallet.Web.Controllers
+{
+    public class BalancesController : Controller
+    {
+        public async Task<IActionResult> Balance()
+        {
+            var address = this.HttpContext.Session.GetString("Address");
+            var nodeUrl = this.HttpContext.Session.GetString("NodeUrl");
+
+            if (string.IsNullOrEmpty(address))
+            {
+                return this.Unauthorized();
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var request = await httpClient.GetAsync($"{nodeUrl}/api/node/balance/address/{address}");
+                if (request.IsSuccessStatusCode)
+                {
+                    var stringResult = await request.Content.ReadAsStringAsync();
+                    var model = JsonConvert.DeserializeObject<AccountBalance>(stringResult);
+
+                    return this.View(model);
+                }
+            }
+
+            return this.RedirectToAction("ImportKeystore", "Account");
+        }
+    }
+}
